deed0e736e6deb9db9215132fb5b0a7ed222e3b5
HervalMata <hervalmataamparo@gmail.com>
2020-11-09 01:00:45 -0300

    Componentes para página

--- lib/src/models/app.dart
+++ lib/src/models/app.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_lacos/src/pages/tela_inicial.dart';
 
 class App extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return MaterialApp(
+      title: 'Laços da Cris',
+      theme: ThemeData(primaryColor: Colors.pinkAccent),
+      home: TelaInicial(),
+    );
   }
 }
--- lib/src/pages/tela_inicial.dart
+++ lib/src/pages/tela_inicial.dart
@@ -1,8 +1,29 @@
 import 'package:flutter/material.dart';
 
-class TelaInicial extends StatelessWidget{
+class TelaInicial extends StatefulWidget{
+  @override
+  _TelaInicialState createState() => _TelaInicialState();
+}
+
+class _TelaInicialState extends State<TelaInicial> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return Scaffold(
+      body: ListView(
+        children: <Widget> [
+          Row(
+            children: <Widget> [
+              Column(
+                children: <Widget> [
+                  Text(
+                    'Laços Fofos da Cris'
+                  )
+                ],
+              )
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
