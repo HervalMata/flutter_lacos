c7f45822db5a35361d73a0fcd03b272ab3f264f3
HervalMata <hervalmataamparo@gmail.com>
2020-11-15 14:51:18 -0300

    Páginas Para as Abas

--- lib/src/tabs/tabs.dart
+++ lib/src/tabs/tabs.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_lacos/src/pages/cart_page.dart';
+import 'package:flutter_lacos/src/pages/categories_page.dart';
+import 'package:flutter_lacos/src/pages/products_page.dart';
+import 'package:flutter_lacos/src/pages/tela_inicial.dart';
 
 class Tabs extends StatefulWidget{
 
@@ -7,6 +11,24 @@ class Tabs extends StatefulWidget{
 }
 
 class _TabsState extends State<Tabs> {
+  int tabNow = 0;
+  TelaInicial telaInicial;
+  CartPage cartPage;
+  ProductsPage productsPage;
+  CategoriesPage categoriesPage;
+  List<Widget> pages;
+  Widget pageNow;
+
+  @override
+  void initState() {
+    telaInicial = TelaInicial();
+    cartPage = CartPage();
+    productsPage = ProductsPage();
+    categoriesPage = CategoriesPage();
+    pages = [telaInicial, cartPage, productsPage, categoriesPage];
+    pageNow = telaInicial;
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
