2c8c44e214433a1f57a7555dc7070cdeee653ed3
HervalMata <hervalmataamparo@gmail.com>
2020-11-15 01:10:04 -0300

    Componente para Abas

---
+++ lib/src/tabs/tabs.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Tabs extends StatefulWidget{
+
+  @override
+  _TabsState createState() => _TabsState();
+}
+
+class _TabsState extends State<Tabs> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
