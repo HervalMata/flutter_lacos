1f82583ee4082ffb9392ca4c653514d8f6dcc007
HervalMata <hervalmataamparo@gmail.com>
2020-11-15 23:58:52 -0300

    Totais Para Carrinho

--- lib/src/pages/cart_page.dart
+++ lib/src/pages/cart_page.dart
@@ -20,6 +20,117 @@ class _CartPageState extends State<CartPage> {
           CartCard(),
         ],
       ),
+      bottomNavigationBar: _TotalContainer(),
+    );
+  }
+
+  Widget _TotalContainer(){
+    return Container(
+      height: 200.0,
+      padding: EdgeInsets.only(
+        left: 10.0, right: 10.0,
+      ),
+      margin: EdgeInsets.only(top: 30.0),
+      child: Column(
+        children: <Widget> [
+          Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget> [
+              Text(
+                "Subtotal",
+                style: TextStyle(
+                  color: Color(0xFF9BA7C6),
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                "25,00",
+                style: TextStyle(
+                  color: Color(0xFF6C6D6D),
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 10.0,),
+          SizedBox(height: 10.0,),
+          Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget> [
+              Text(
+                "Taxa de Entrega",
+                style: TextStyle(
+                  color: Color(0xFF9BA7C6),
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                "3,00",
+                style: TextStyle(
+                  color: Color(0xFF6C6D6D),
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 10.0,),
+          SizedBox(height: 2.0,),
+          SizedBox(height: 20.0,),
+          Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget> [
+              Text(
+                "Total",
+                style: TextStyle(
+                  color: Color(0xFF9BA7C6),
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                "28,00",
+                style: TextStyle(
+                  color: Color(0xFF6C6D6D),
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 20.0,),
+          GestureDetector(
+            onTap: (){
+
+            },
+            child: Container(
+              height: 50.0,
+              decoration: BoxDecoration(
+                color: Theme.of(context).primaryColor,
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: Center(
+                child: Text(
+                  "Finalizar Pagamento",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 10.0,),
+        ],
+      ),
     );
   }
 }
+
