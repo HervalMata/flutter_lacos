f8a23fee2f5d8b0f7665ce93d4098d5b9c52839a
HervalMata <hervalmataamparo@gmail.com>
2020-11-16 00:45:13 -0300

    Campos Para o Login

--- lib/src/pages/login_page.dart
+++ lib/src/pages/login_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_lacos/src/components/button.dart';
 
 class LoginPage extends StatefulWidget{
 
@@ -36,7 +37,7 @@ class _LoginPageState extends State<LoginPage> {
                 : Icon(Icons.visibility),
             onPressed: (){
               setState(() {
-
+                _toggleVisibility = !_toggleVisibility;
               });
             }
         )
@@ -47,7 +48,40 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 10.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget> [
+            Image(
+                image: AssetImage("assets/images/login.gif"),
+                height: 140.0,
+                width: 140.0,
+            ),
+            SizedBox(height: 15.0,),
+            SizedBox(height: 20.0,),
+            Card(
+              elevation: 5.0,
+              child: Padding(
+                  padding: EdgeInsets.all(20.0),
+                  child: Column(
+                    children: <Widget> [
+                      _emailTextField(),
+                      SizedBox(height: 20.0,),
+                      _passwordTextField(),
+                    ],
+                  ),
+              ),
+            ),
+            SizedBox(height: 30.0,),
+            Button(btnText: "Logar"),
+            Divider(
+              height: 20.0,
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
