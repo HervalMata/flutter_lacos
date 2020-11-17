41eaea4e8d00f2113ae51414994fa37b7cc4e9e1
HervalMata <hervalmataamparo@gmail.com>
2020-11-16 21:12:54 -0300

    Listar categorias

---
+++ api/products/categories.php
@@ -0,0 +1,21 @@
+<?php
+  include_once("../connection.php");
+
+  $data = array();
+  
+  $query = $pdo->query("SELECT * FROM categories");
+  
+  $res = $query->fetchAll(PDO::FETCH_ASSOC);
+
+  for ($i=0; $i < count($res); $i++) { 
+    foreach ($res[$i] as $key => $value) {
+      
+    }
+    $data = $res;
+  }
+
+  echo ($res) ?
+  json_encode(array("code" => 1, "result" => $data)) :
+  json_encode(array("code" => 0, "result" => "Dados não encontrados"))
+
+?>
